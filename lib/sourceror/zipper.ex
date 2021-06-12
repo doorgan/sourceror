8b6b908b59512418e0201d420b0a5898b481a4bd
Dorgan <dorgandash@gmail.com>
2021-06-12 11:53:46 -0300

    Add Zippers (#10)
    * Add Sourceror.Zipper
    
    * Add zippers livebook

---
+++ lib/sourceror/zipper.ex
@@ -0,0 +1,382 @@
+defmodule Sourceror.Zipper do
+  @moduledoc """
+  Implements a Zipper for the Elixir AST based on Gérard Huet [Functional
+  pearl: the
+  zipper](https://www.st.cs.uni-saarland.de/edu/seminare/2005/advanced-fp/docs/huet-zipper.pdf)
+  paper.
+  """
+
+  # Remove once we figure out why these functions cause a "pattern can never
+  # match" error:
+  #
+  # The pattern can never match the type.
+  #
+  # Pattern: _child = {_, _}
+  #
+  # Type: nil
+  @dialyzer {:nowarn_function, do_prev: 1, prev_after_remove: 1}
+
+  import Kernel, except: [node: 1]
+
+  @type tree :: Macro.t()
+  @type path :: %{
+          l: [tree],
+          ptree: zipper,
+          r: [tree]
+        }
+  @type zipper :: {tree, path | nil | :end}
+
+  @doc """
+  Returns true if the node is a branch.
+  """
+  @spec branch?(tree) :: boolean
+  def branch?({_, _, args}) when is_list(args), do: true
+  def branch?({_, _}), do: true
+  def branch?(list) when is_list(list), do: true
+  def branch?(_), do: false
+
+  @doc """
+  Returns a list of children of the node.
+  """
+  @spec children(tree) :: [tree]
+  def children({form, _, args}) when is_atom(form) and is_list(args), do: args
+  def children({form, _, args}) when is_list(args), do: [form | args]
+  def children({left, right}), do: [left, right]
+  def children(list) when is_list(list), do: list
+
+  @doc """
+  Returns a new branch node, given an existing node and new children.
+  """
+  @spec make_node(tree, [tree]) :: tree
+
+  def make_node({form, meta, _}, args) when is_atom(form), do: {form, meta, args}
+
+  def make_node({_form, meta, args}, [first | rest]) when is_list(args), do: {first, meta, rest}
+
+  def make_node({_, _}, [left, right]), do: {left, right}
+  def make_node({_, _}, args), do: {:{}, [], args}
+
+  def make_node(list, children) when is_list(list), do: children
+
+  @doc """
+  Creates a zipper from a tree node.
+  """
+  @spec zip(tree) :: zipper
+  def zip(term), do: {term, nil}
+
+  @doc """
+  Walks the zipper all the way up and returns the top zipper.
+  """
+  @spec top(zipper) :: zipper
+  def top({tree, :end}), do: {tree, :end}
+
+  def top(zipper) do
+    if parent = up(zipper) do
+      top(parent)
+    else
+      zipper
+    end
+  end
+
+  @doc """
+  Walks the zipper all the way up and returns the root node.
+  """
+  @spec root(zipper) :: tree
+  def root(zipper), do: zipper |> top() |> node()
+
+  @doc """
+  Returns the node at the zipper.
+  """
+  @spec node(zipper) :: tree
+  def node({tree, _}), do: tree
+
+  @doc """
+  Returns the zipper of the leftmost child of the node at this zipper, or
+  nil if no there's no children.
+  """
+  @spec down(zipper) :: zipper | nil
+  def down({tree, meta}) do
+    with true <- branch?(tree), [first | rest] <- children(tree) do
+      {first, %{ptree: {tree, meta}, l: nil, r: rest}}
+    else
+      _ -> nil
+    end
+  end
+
+  @doc """
+  Returns the zipper of the parent of the node at this zipper, or nil if at the
+  top.
+  """
+  @spec up(zipper) :: zipper | nil
+  def up({_, nil}), do: nil
+
+  def up({tree, meta}) do
+    children = Enum.reverse(meta.l || []) ++ [tree] ++ (meta.r || [])
+    {parent, parent_meta} = meta.ptree
+    {make_node(parent, children), parent_meta}
+  end
+
+  @doc """
+  Returns the zipper of the left sibling of the node at this zipper, or nil.
+  """
+  @spec left(zipper) :: zipper | nil
+  def left({_, nil}), do: nil
+  def left({_, %{l: nil}}), do: nil
+
+  def left({tree, meta}) do
+    r = [tree | meta.r || []]
+
+    case meta.l do
+      [tree | l] ->
+        {tree, %{meta | l: l, r: r}}
+
+      [] ->
+        nil
+    end
+  end
+
+  @doc """
+  Returns the leftmost sibling of the node at this zipper, or itself.
+  """
+  @spec leftmost(zipper) :: zipper
+  def leftmost({_, nil} = zipper), do: zipper
+  def leftmost({_, %{l: nil}} = zipper), do: zipper
+
+  def leftmost({tree, meta}) do
+    [left | rest] = Enum.reverse(meta.l)
+    r = rest ++ [tree] ++ (meta.r || [])
+
+    {left, %{meta | l: nil, r: r}}
+  end
+
+  @doc """
+  "Returns the zipper of the right sibling of the node at this zipper, or nil.
+  """
+  @spec right(zipper) :: zipper | nil
+  def right({_, nil}), do: nil
+  def right({_, %{r: nil}}), do: nil
+
+  def right({tree, meta}) do
+    l = [tree | meta.l || []]
+
+    case meta.r do
+      [tree | r] ->
+        {tree, %{meta | l: l, r: r}}
+
+      [] ->
+        nil
+    end
+  end
+
+  @doc """
+  Returns the rightmost sibling of the node at this zipper, or itself.
+  """
+  @spec rightmost(zipper) :: zipper
+  def rightmost({_, nil} = zipper), do: zipper
+  def rightmost({_, %{r: nil}} = zipper), do: zipper
+
+  def rightmost({tree, meta}) do
+    [right | rest] = Enum.reverse(meta.r)
+    l = rest ++ [tree] ++ (meta.l || [])
+
+    {right, %{meta | l: l, r: nil}}
+  end
+
+  @doc """
+  Replaces the current node in the zipper with a new node.
+  """
+  @spec replace(zipper, tree) :: zipper
+  def replace({_, meta}, tree), do: {tree, meta}
+
+  @doc """
+  Replaces the current node in the zipper with the result of applying `fun` to
+  the node.
+  """
+  @spec update(zipper, (tree -> tree)) :: zipper
+  def update({tree, meta}, fun), do: {fun.(tree), meta}
+
+  @doc """
+  Removes the node at the zipper, returning the zipper that would have preceded
+  it in a depth-first walk.
+  """
+  @spec remove(zipper) :: zipper
+  def remove({_, nil}), do: raise(ArgumentError, message: "Cannot remove the top level node.")
+
+  def remove({_, meta}) do
+    case meta.l do
+      [left | rest] ->
+        prev_after_remove({left, %{meta | l: rest}})
+
+      _ ->
+        children = meta.r || []
+        {parent, parent_meta} = meta.ptree
+        {make_node(parent, children), parent_meta}
+    end
+  end
+
+  defp prev_after_remove(zipper) do
+    with true <- branch?(node(zipper)),
+         {_, _} = child <- down(zipper) do
+      prev_after_remove(rightmost(child))
+    else
+      _ -> zipper
+    end
+  end
+
+  @doc """
+  Inserts the item as the left sibling of the node at this zipper, without
+  moving. Raises an `ArgumentError` when attempting to insert a sibling at the
+  top level.
+  """
+  @spec insert_left(zipper, tree) :: zipper
+  def insert_left({_, nil}, _),
+    do: raise(ArgumentError, message: "Can't insert siblings at the top level.")
+
+  def insert_left({tree, meta}, child) do
+    {tree, %{meta | l: [child | meta.l || []]}}
+  end
+
+  @doc """
+  Inserts the item as the right sibling of the node at this zipper, without
+  moving. Raises an `ArgumentError` when attempting to insert a sibling at the
+  top level.
+  """
+  @spec insert_right(zipper, tree) :: zipper
+  def insert_right({_, nil}, _),
+    do: raise(ArgumentError, message: "Can't insert siblings at the top level.")
+
+  def insert_right({tree, meta}, child) do
+    {tree, %{meta | r: [child | meta.r || []]}}
+  end
+
+  @doc """
+  Inserts the item as the leftmost child of the node at this zipper,
+  without moving.
+  """
+  def insert_child({tree, meta}, child) do
+    {do_insert_child(tree, child), meta}
+  end
+
+  @doc """
+  Inserts the item as the rightmost child of the node at this zipper,
+  without moving.
+  """
+  def append_child({tree, meta}, child) do
+    {do_append_child(tree, child), meta}
+  end
+
+  @doc """
+  Returns true if the zipper represents the end of a depth-first walk.
+  """
+  @spec end?(zipper) :: boolean
+  def end?({_, meta}), do: meta == :end
+
+  @doc """
+  Returns the following zipper in depth-first pre-order. When reaching the end,
+  returns a distinguished zipper detectable via `end?/1`. If it's already at
+  the end, it stays there.
+  """
+  def next({_, :end} = zipper), do: zipper
+
+  def next({tree, _} = zipper) do
+    cond do
+      branch?(tree) && down(zipper) ->
+        down(zipper)
+
+      next = right(zipper) ->
+        next
+
+      true ->
+        next_up(zipper)
+    end
+  end
+
+  defp next_up(zipper) do
+    parent = up(zipper)
+
+    if parent do
+      right(parent) || next_up(parent)
+    else
+      {node(zipper), :end}
+    end
+  end
+
+  @doc """
+  Returns the previous zipper in depth-first pre-order. If it's already at
+  the end, it returns nil.
+  """
+  @spec prev(zipper) :: zipper
+  def prev(zipper) do
+    if left = left(zipper) do
+      do_prev(left)
+    else
+      up(zipper)
+    end
+  end
+
+  defp do_prev(zipper) do
+    with true <- branch?(node(zipper)),
+         {_, _} = child <- down(zipper) do
+      do_prev(rightmost(child))
+    else
+      _ -> zipper
+    end
+  end
+
+  @doc """
+  Traverses the tree in depth-first pre-order calling the given function for
+  each node. Returns a zipper to the root node.
+
+  The function must return a zipper.
+  """
+  @spec traverse(zipper, (zipper -> zipper)) :: zipper
+  def traverse({tree, :end}, _), do: {tree, :end}
+
+  def traverse(zipper, fun) do
+    fun.(zipper)
+    |> next()
+    |> traverse(fun)
+  end
+
+  @doc """
+  Traverses the tree in depth-first pre-order calling the given function for
+  each node with an accumulator. Returns a zipper to the root node and the
+  accumulator.
+  """
+  @spec traverse(zipper, term, (zipper, term -> {zipper, term})) :: {zipper, term}
+  def traverse({tree, :end}, acc, _), do: {{tree, :end}, acc}
+
+  def traverse(zipper, acc, fun) do
+    {zipper, acc} = fun.(zipper, acc)
+
+    traverse(next(zipper), acc, fun)
+  end
+
+  @doc """
+  Returns a zipper to the node that satisfies the predicate function, or `nil`
+  if none is found.
+  """
+  def find({_, :end}, _), do: nil
+
+  def find({tree, _} = zipper, predicate) do
+    if predicate.(tree) do
+      zipper
+    else
+      find(next(zipper), predicate)
+    end
+  end
+
+  defp do_insert_child({form, meta, args}, child) when is_list(args) do
+    {form, meta, [child | args]}
+  end
+
+  defp do_insert_child(list, child) when is_list(list), do: [child | list]
+  defp do_insert_child({left, right}, child), do: {:{}, [], [child, left, right]}
+
+  defp do_append_child({form, meta, args}, child) when is_list(args) do
+    {form, meta, args ++ [child]}
+  end
+
+  defp do_append_child(list, child) when is_list(list), do: list ++ [child]
+  defp do_append_child({left, right}, child), do: {:{}, [], [left, right, child]}
+end
---
+++ test/zipper_test.exs
@@ -0,0 +1,377 @@
+defmodule SourcerorTest.ZipperTest do
+  use ExUnit.Case, async: true
+  doctest Sourceror.Zipper
+
+  alias Sourceror.Zipper, as: Z
+
+  describe "zip/1" do
+    test "creates a zipper from a term" do
+      assert Z.zip(42) == {42, nil}
+    end
+  end
+
+  describe "branch?/1" do
+    test "correctly identifies branch nodes" do
+      assert Z.branch?(42) == false
+      assert Z.branch?(:foo) == false
+      assert Z.branch?([1, 2, 3]) == true
+      assert Z.branch?({:left, :right}) == true
+      assert Z.branch?({:foo, [], []}) == true
+    end
+  end
+
+  describe "children/1" do
+    test "returns the children for a node" do
+      assert Z.children([1, 2, 3]) == [1, 2, 3]
+      assert Z.children({:foo, [], [1, 2]}) == [1, 2]
+
+      assert Z.children({{:., [], [:left, :right]}, [], [:arg]}) == [
+               {:., [], [:left, :right]},
+               :arg
+             ]
+
+      assert Z.children({:left, :right}) == [:left, :right]
+    end
+  end
+
+  describe "make_node/2" do
+    test "2-tuples" do
+      assert Z.make_node({1, 2}, [3, 4]) == {3, 4}
+    end
+
+    test "changing to 2-tuples arity" do
+      assert Z.make_node({1, 2}, [3, 4, 5]) == {:{}, [], [3, 4, 5]}
+      assert Z.make_node({1, 2}, [3]) == {:{}, [], [3]}
+    end
+
+    test "lists" do
+      assert Z.make_node([1, 2, 3], [:a, :b, :c]) == [:a, :b, :c]
+    end
+
+    test "unqualified calls" do
+      assert Z.make_node({:foo, [], [1, 2]}, [:a, :b]) == {:foo, [], [:a, :b]}
+    end
+
+    test "qualified calls" do
+      assert Z.make_node({{:., [], [1, 2]}, [], [3, 4]}, [:a, :b, :c]) == {:a, [], [:b, :c]}
+    end
+  end
+
+  describe "node/1" do
+    test "returns the node for a zipper" do
+      assert Z.node(Z.zip(42)) == 42
+    end
+  end
+
+  describe "down/1" do
+    test "rips and tears the parent node" do
+      assert Z.zip([1, 2]) |> Z.down() == {1, %{l: nil, r: [2], ptree: {[1, 2], nil}}}
+      assert Z.zip({1, 2}) |> Z.down() == {1, %{l: nil, r: [2], ptree: {{1, 2}, nil}}}
+
+      assert Z.zip({:foo, [], [1, 2]}) |> Z.down() ==
+               {1, %{l: nil, r: [2], ptree: {{:foo, [], [1, 2]}, nil}}}
+
+      assert Z.zip({{:., [], [:a, :b]}, [], [1, 2]}) |> Z.down() ==
+               {{:., [], [:a, :b]},
+                %{l: nil, r: [1, 2], ptree: {{{:., [], [:a, :b]}, [], [1, 2]}, nil}}}
+    end
+  end
+
+  describe "up/1" do
+    test "reconstructs the previous parent" do
+      assert Z.zip([1, 2]) |> Z.down() |> Z.up() == {[1, 2], nil}
+      assert Z.zip({1, 2}) |> Z.down() |> Z.up() == {{1, 2}, nil}
+      assert Z.zip({:foo, [], [1, 2]}) |> Z.down() |> Z.up() == {{:foo, [], [1, 2]}, nil}
+
+      assert Z.zip({{:., [], [:a, :b]}, [], [1, 2]}) |> Z.down() |> Z.up() ==
+               {{{:., [], [:a, :b]}, [], [1, 2]}, nil}
+    end
+
+    test "returns nil at the top level" do
+      assert Z.zip(42) |> Z.up() == nil
+    end
+  end
+
+  describe "left/1 and right/1" do
+    test "correctly navigate horizontally" do
+      zipper = Z.zip([1, [2, 3], [[4, 5], 6]])
+
+      assert zipper |> Z.down() |> Z.right() |> Z.right() |> Z.node() == [[4, 5], 6]
+      assert zipper |> Z.down() |> Z.right() |> Z.right() |> Z.left() |> Z.node() == [2, 3]
+    end
+
+    test "return nil at the boundaries" do
+      zipper = Z.zip([1, 2])
+
+      assert zipper |> Z.down() |> Z.left() == nil
+      assert zipper |> Z.down() |> Z.right() |> Z.right() == nil
+    end
+  end
+
+  describe "rightmost/1" do
+    test "returns the rightmost child" do
+      assert Z.zip([1, 2, 3, 4, 5]) |> Z.down() |> Z.rightmost() |> Z.node() == 5
+    end
+
+    test "returns itself it already at the rightmost node" do
+      assert Z.zip([1, 2, 3, 4, 5])
+             |> Z.down()
+             |> Z.rightmost()
+             |> Z.rightmost()
+             |> Z.rightmost()
+             |> Z.node() == 5
+
+      assert Z.zip([1, 2, 3])
+             |> Z.rightmost()
+             |> Z.rightmost()
+             |> Z.node() == [1, 2, 3]
+    end
+  end
+
+  describe "leftmost/1" do
+    test "returns the leftmost child" do
+      assert Z.zip([1, 2, 3, 4, 5])
+             |> Z.down()
+             |> Z.right()
+             |> Z.right()
+             |> Z.leftmost()
+             |> Z.node() == 1
+    end
+
+    test "returns itself it already at the leftmost node" do
+      assert Z.zip([1, 2, 3, 4, 5])
+             |> Z.down()
+             |> Z.leftmost()
+             |> Z.leftmost()
+             |> Z.leftmost()
+             |> Z.node() == 1
+
+      assert Z.zip([1, 2, 3])
+             |> Z.leftmost()
+             |> Z.leftmost()
+             |> Z.node() == [1, 2, 3]
+    end
+  end
+
+  describe "next/1" do
+    test "walks forward in depth-first pre-order" do
+      zipper = Z.zip([1, [2, [3, 4]], 5])
+
+      assert zipper |> Z.next() |> Z.next() |> Z.next() |> Z.next() |> Z.node() == [3, 4]
+
+      assert zipper
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.node() == 5
+    end
+
+    test "sets meta to :end when finishes" do
+      zipper = Z.zip([1, [2, [3, 4]], 5])
+
+      assert {_, :end} =
+               zipper
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+               |> Z.next()
+
+      assert {42, :end} |> Z.next() == {42, :end}
+    end
+  end
+
+  describe "prev/1" do
+    test "walks backwards in depth-first pre-order" do
+      zipper = Z.zip([1, [2, [3, 4]], 5])
+
+      assert zipper
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.prev()
+             |> Z.prev()
+             |> Z.prev()
+             |> Z.node() == [3, 4]
+    end
+
+    test "returns nil when it reaches past the top" do
+      zipper = Z.zip([1, [2, [3, 4]], 5])
+
+      assert zipper
+             |> Z.next()
+             |> Z.next()
+             |> Z.next()
+             |> Z.prev()
+             |> Z.prev()
+             |> Z.prev()
+             |> Z.prev() == nil
+    end
+  end
+
+  describe "end?/1" do
+    test "returns true if it's an end zipper" do
+      assert Z.end?({42, nil}) == false
+      assert Z.end?({42, :end}) == true
+    end
+  end
+
+  describe "traverse/2" do
+    test "traverses in depth-first pre-order" do
+      zipper = Z.zip([1, [2, [3, 4], 5], [6, 7]])
+
+      assert Z.traverse(zipper, fn
+               {x, m} when is_integer(x) -> {x * 2, m}
+               z -> z
+             end)
+             |> Z.node() == [2, [4, [6, 8], 10], [12, 14]]
+    end
+  end
+
+  describe "traverse/3" do
+    test "traverses in depth-first pre-order" do
+      zipper = Z.zip([1, [2, [3, 4], 5], [6, 7]])
+
+      {_, acc} = Z.traverse(zipper, [], &{&1, [Z.node(&1) | &2]})
+
+      assert [
+               [1, [2, [3, 4], 5], [6, 7]],
+               1,
+               [2, [3, 4], 5],
+               2,
+               [3, 4],
+               3,
+               4,
+               5,
+               [6, 7],
+               6,
+               7
+             ] == Enum.reverse(acc)
+    end
+  end
+
+  describe "top/1" do
+    test "returns the top zipper" do
+      assert Z.zip([1, [2, [3, 4]]]) |> Z.next() |> Z.next() |> Z.next() |> Z.top() ==
+               {[1, [2, [3, 4]]], nil}
+
+      assert Z.zip(42) |> Z.top() |> Z.top() |> Z.top() == {42, nil}
+      assert {42, :end} |> Z.top() |> Z.top() |> Z.top() == {42, :end}
+    end
+  end
+
+  describe "root/1" do
+    test "returns the root node" do
+      assert Z.zip([1, [2, [3, 4]]]) |> Z.next() |> Z.next() |> Z.next() |> Z.root() ==
+               [1, [2, [3, 4]]]
+    end
+  end
+
+  describe "replace/2" do
+    test "replaces the current node" do
+      assert Z.zip([1, 2]) |> Z.down() |> Z.replace(:a) |> Z.root() == [:a, 2]
+    end
+  end
+
+  describe "update/2" do
+    test "updates the current node" do
+      assert Z.zip([1, 2]) |> Z.down() |> Z.update(fn x -> x + 50 end) |> Z.root() ==
+               [51, 2]
+    end
+  end
+
+  describe "remove/1" do
+    test "removes the node and goes back to the previous zipper" do
+      zipper = Z.zip([1, [2, 3], 4]) |> Z.down() |> Z.rightmost() |> Z.remove()
+
+      assert Z.node(zipper) == 3
+      assert Z.root(zipper) == [1, [2, 3]]
+
+      assert Z.zip([1, 2, 3])
+             |> Z.next()
+             |> Z.rightmost()
+             |> Z.remove()
+             |> Z.remove()
+             |> Z.remove()
+             |> Z.node() == []
+    end
+
+    test "raises when attempting to remove the root" do
+      assert_raise ArgumentError, fn ->
+        Z.zip(42) |> Z.remove()
+      end
+    end
+  end
+
+  describe "insert_left/2 and insert_right/2" do
+    test "insert a sibling to the left or right" do
+      assert Z.zip([1, 2, 3])
+             |> Z.down()
+             |> Z.right()
+             |> Z.insert_left(:left)
+             |> Z.insert_right(:right)
+             |> Z.root() == [1, :left, 2, :right, 3]
+    end
+
+    test "raise when attempting to insert a sibling at the root" do
+      assert_raise ArgumentError, fn -> Z.zip(42) |> Z.insert_left(:nope) end
+      assert_raise ArgumentError, fn -> Z.zip(42) |> Z.insert_right(:nope) end
+    end
+  end
+
+  describe "insert_child/2 and append_child/2" do
+    test "add child nodes to the leftmost or rightmost side" do
+      assert Z.zip([1, 2, 3]) |> Z.insert_child(:first) |> Z.append_child(:last) |> Z.root() == [
+               :first,
+               1,
+               2,
+               3,
+               :last
+             ]
+
+      assert Z.zip({:left, :right}) |> Z.insert_child(:first) |> Z.root() ==
+               {:{}, [],
+                [
+                  :first,
+                  :left,
+                  :right
+                ]}
+
+      assert Z.zip({:left, :right}) |> Z.append_child(:last) |> Z.root() ==
+               {:{}, [],
+                [
+                  :left,
+                  :right,
+                  :last
+                ]}
+
+      assert Z.zip({:foo, [], []}) |> Z.insert_child(:first) |> Z.append_child(:last) |> Z.root() ==
+               {:foo, [], [:first, :last]}
+
+      assert Z.zip({{:., [], [:a, :b]}, [], []})
+             |> Z.insert_child(:first)
+             |> Z.append_child(:last)
+             |> Z.root() ==
+               {{:., [], [:a, :b]}, [], [:first, :last]}
+    end
+  end
+
+  describe "find/2" do
+    test "finds a zipper with a predicate" do
+      zipper = Z.zip([1, [2, [3, 4], 5]])
+
+      assert Z.find(zipper, fn x -> x == 4 end) |> Z.node() == 4
+    end
+  end
+end
