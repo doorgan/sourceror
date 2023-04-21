6589d815a1aacc02045a193e7b74e713af5d0517
Matt Enlow <enlow@adobe.com>
2023-04-21 17:06:30 -0600

    Zipper: Fix traverse_while started from a zipper that isn't at the top (#93)
    * Zipper: Fix traverse_while started from a zipper that isn't at the top
    
    * update the other traverse_while as well

--- lib/sourceror/zipper.ex
+++ lib/sourceror/zipper.ex
@@ -375,7 +375,7 @@ defmodule Sourceror.Zipper do
   end
 
   def traverse_while({tree, meta}, fun) do
-    {updated, _meta} = do_traverse({tree, nil}, fun)
+    {updated, _meta} = do_traverse_while({tree, nil}, fun)
     {updated, meta}
   end
 
@@ -412,7 +412,7 @@ defmodule Sourceror.Zipper do
   end
 
   def traverse_while({tree, meta}, acc, fun) do
-    {{updated, _meta}, acc} = do_traverse({tree, nil}, acc, fun)
+    {{updated, _meta}, acc} = do_traverse_while({tree, nil}, acc, fun)
     {{updated, meta}, acc}
   end
 
